90017e18c93b7549f3fefb850736b53e20c40bc7
GrandpaNewton <ivan.gromyko.2804@gmail.com>
2024-04-27 11:18:33 +0300

    Code refactoring

--- Lab02/Game.cs
+++ Lab02/Game.cs
@@ -127,8 +127,18 @@ namespace QuestGame
         private Texture _stoneTexture;
         
         
+        private bool _firstRun = true;
+        private bool _isDisplayingText;
+        
+        private TextLayout _textLayout = null;
+        private TextFormat testTextFormat;
+        private readonly MainInventoryItem treasureItem;
+        private readonly MeshObject _secondNpcObject;
+        private XAudio2 xaudio2;
+        private static SourceVoice sourceVoice;
+        private static AudioBuffer audioBuffer;
 
-        public Game()
+        private void CreatingObjects()
         {
             _renderForm = new RenderForm();
             _renderForm.UserResized += RenderFormResizedCallback;
@@ -196,8 +206,20 @@ namespace QuestGame
             _plotSize = new Vector2(5.0f, 5.0f);
 
             _light.GlobalAmbient = new Vector4(0.5f, 0.5f, 0.5f, 1.0f);
+        }
+
+        private void LoadingObjectsAndColliders(Loader loader)
+        {
+            
+        }
+        public Game()
+        {
+            CreatingObjects();
 
             Loader loader = new Loader(_directX3DGraphics);
+            
+            
+            // ***
             _tetrahedronTexture = loader.LoadTextureFromFile("greyTexture.jpg", _renderer.AnisotropicSampler);
             _npcTexture = loader.LoadTextureFromFile("texture.bmp", _renderer.AnisotropicSampler);
             _plotTexture = loader.LoadTextureFromFile("edward-godlach-screenshot003.jpg", _renderer.AnisotropicSampler);
@@ -226,12 +248,10 @@ namespace QuestGame
             
             var secondNpcCollider = new BoundingBox(new Vector3(result.Vertices.Min(v => v.X), result.Vertices.Min(v => v.Y), result.Vertices.Min(v => v.Z)) + (Vector3)_secondNpcObject.Position,
                 new Vector3(result.Vertices.Max(v => v.X), result.Vertices.Max(v => v.Y), result.Vertices.Max(v => v.Z)) + (Vector3)_secondNpcObject.Position);
-
-           
-            
             
             _treasureCollider = new BoundingBox(_firstNpcCollider.Minimum + ToDecart(new Vector3(1, 1, 0)), _firstNpcCollider.Maximum + ToDecart(new Vector3(1, 1, 0)));
 
+            // ***
 
             MainInventoryItem shovelItem = new MainInventoryItem(new Sprite(_directX3DGraphics,
                 DirectX3DGraphics.LoadFromFile(_directX3DGraphics.D2DRenderTarget, "shovel.png"), Vector2.Zero, 0f,
@@ -466,17 +486,6 @@ namespace QuestGame
             _camera.Height = _renderForm.ClientSize.Height;
         }
 
-        private bool _firstRun = true;
-        private bool _isDisplayingText;
-        
-        private TextLayout _textLayout = null;
-        private TextFormat testTextFormat;
-        private readonly MainInventoryItem treasureItem;
-        private readonly MeshObject _secondNpcObject;
-        private XAudio2 xaudio2;
-        private static SourceVoice sourceVoice;
-        private static AudioBuffer audioBuffer;
-
         private void CheckSecondQuestCompleting()
         {
             if (_secondNpcSequentialQuest.IsStarting)
@@ -509,24 +518,8 @@ namespace QuestGame
             }
         }
 
-        public void RenderLoopCallBack()
+        private void UpdateInput()
         {
-            if (_firstRun)
-            {
-                RenderFormResizedCallback(this, EventArgs.Empty);
-                _firstRun = false;
-            }
-            
-            _timeHelper.Update();
-            _renderForm.Text = "FPS: " + _timeHelper.FPS.ToString();
-            //_player.YawBy(_timeHelper.DeltaT * MathUtil.TwoPi * 0.1f);
-
-            _dxInput.Update();
-
-            /*_camera.YawBy(_dxInput.GetMouseDeltaX() * 0.0005f);
-            _camera.PitchBy(_dxInput.GetMouseDeltaY() * 0.0001f);*/
-
-
             Vector3 playerMovement = Vector3.Zero;
 
             float deltaYaw = 0f;
@@ -571,10 +564,12 @@ namespace QuestGame
             _playerCollider.Minimum += playerMovement;
             _playerCollider.Maximum += playerMovement;
             _camera.MoveBy(playerMovement.X, playerMovement.Y, playerMovement.Z);
-            
-            
-            
-            PressKeyboard();
+
+
+        }
+
+        private void RenderObjects()
+        {
             Matrix viewMatrix = _camera.GetViewMatrix();
             Matrix projectionMatrix = _camera.GetProjectionMatrix();
             _light.EyePosition = _camera.Position;
@@ -636,7 +631,10 @@ namespace QuestGame
                         activeItem.Item.Interact();
                 }
             }
-            
+        }
+
+        private void DrawHUD()
+        {
             testTextFormat = new TextFormat(_directX3DGraphics.FactoryDWrite, "Calibri", 28)
             {
                 TextAlignment = SharpDX.DirectWrite.TextAlignment.Center,
@@ -644,12 +642,75 @@ namespace QuestGame
             };
 
             _directX3DGraphics.D2DRenderTarget.TextAntialiasMode = SharpDX.Direct2D1.TextAntialiasMode.Cleartype;
-                 //= new TextLayout(_directX3DGraphics.FactoryDWrite, $"NPC:{_firstNpcCollider.Center},\n\r Player:{_playerCollider.Center}", testTextFormat, _renderForm.Width, _renderForm.Height);
+            //= new TextLayout(_directX3DGraphics.FactoryDWrite, $"NPC:{_firstNpcCollider.Center},\n\r Player:{_playerCollider.Center}", testTextFormat, _renderForm.Width, _renderForm.Height);
 
             _directX3DGraphics.D2DRenderTarget.BeginDraw();
 
-            CheckSecondQuestCompleting();
-            
+            if (_isOpenCollectibleItems)
+            {
+                _directX3DGraphics.D2DRenderTarget.FillRectangle(new SharpDX.Mathematics.Interop.RawRectangleF(0f, 0f, _renderForm.Width, _renderForm.Height), _blackBrush); // рамка карты
+                _collectiblesInventory.DrawInventory();
+            }
+            else
+            {
+                _mainInventory.DrawInventory();
+            }
+
+            if (_isDisplayingText)
+            {
+                //textLayoutText = (_secondNpcSequentialQuest.InteractableObjects[0].MeshCollider.Center).ToString() + _player.Position;
+                _textLayout = new TextLayout(_directX3DGraphics.FactoryDWrite, textLayoutText, testTextFormat, _renderForm.Width, _renderForm.Height);
+                _directX3DGraphics.D2DRenderTarget.DrawTextLayout(new RawVector2(_renderForm.Width / 2 - 400, _renderForm.Height / 2 - 100), _textLayout, _whiteBrush, DrawTextOptions.None);
+            }
+
+            _directX3DGraphics.D2DRenderTarget.EndDraw();
+            _directX3DGraphics.SwapChain.Present(0, PresentFlags.None);
+        }
+
+        private void InteractWithNpc(KeyValuePair<BoundingBox, Npc> npc)
+        {
+            _isDisplayingText = true;
+            NpcResponse npcResponse = npc.Value.Interact();
+            textLayoutText = npcResponse.ResponseText;
+                        
+            if (npc.Value.NpcState == NpcStates.AfterQuestComplete)
+            {
+                var takenItems = npcResponse.Items;
+
+                if (takenItems != null)
+                {
+                    for (int i = 0; i < takenItems.Length; i++)
+                    {
+                        _mainInventory.RemoveItem(takenItems[i]);
+                    }
+                }
+                            
+                var collectibles = npc.Value.GetCollectibles();
+                if (collectibles != null)
+                {
+                    for (int i = 0; i < collectibles.Length; i++)
+                    {
+                        _collectiblesInventory.AddItem(collectibles[i]);
+                    }
+                }
+
+                npcSpeed = ToDecart(new Vector3(0f, 0.0001f, 0f));
+            }
+            else
+            {
+                var items = npcResponse.Items;
+                            
+                if (items != null)
+                {
+                    for (int i = 0; i < items.Length; i++)
+                    {
+                        _mainInventory.AddItem(items[i]);
+                    }
+                }
+            }
+        }
+        private void UpdateNPC()
+        {
             if (_isDisplayingText && _dxInput.IsKeyReleased(Key.E))
             {
                 _isDisplayingText = false;
@@ -661,188 +722,38 @@ namespace QuestGame
                 {
                     if (npc.Key.Intersects(_playerCollider))
                     {
-                        _isDisplayingText = true;
-                        NpcResponse npcResponse = npc.Value.Interact();
-                        textLayoutText = npcResponse.ResponseText;
-                        
-                        if (npc.Value.NpcState == NpcStates.AfterQuestComplete)
-                        {
-                            var takenItems = npcResponse.Items;
-
-                            if (takenItems != null)
-                            {
-                                for (int i = 0; i < takenItems.Length; i++)
-                                {
-                                    _mainInventory.RemoveItem(takenItems[i]);
-                                }
-                            }
-                            
-                            var collectibles = npc.Value.GetCollectibles();
-                            if (collectibles != null)
-                            {
-                                for (int i = 0; i < collectibles.Length; i++)
-                                {
-                                    _collectiblesInventory.AddItem(collectibles[i]);
-                                }
-                            }
-
-                            npcSpeed = ToDecart(new Vector3(0f, 0.0001f, 0f));
-                        }
-                        else
-                        {
-                            var items = npcResponse.Items;
-                            
-                            if (items != null)
-                            {
-                                for (int i = 0; i < items.Length; i++)
-                                {
-                                    _mainInventory.AddItem(items[i]);
-                                }
-                            }
-                        }
+                        InteractWithNpc(npc);
                     }
                 }
             }
             
             npcColliders[_firstNpcCollider].GameObject.MoveBy(npcSpeed.X, npcSpeed.Y, npcSpeed.Z);
 
-            /*if (_firstNpcCollider.Intersects(_playerCollider) && _dxInput.IsKeyReleased(Key.E))
+        }
+        
+        public void RenderLoopCallBack()
+        {
+            if (_firstRun)
             {
-                
-                textLayout = new TextLayout(_directX3DGraphics.FactoryDWrite, $"Здарова, мужик, вот те карта, найди там мой сундук, \r\nа я тебе трофейчик небольшой дам, давай, на связи", testTextFormat, _renderForm.Width, _renderForm.Height);
-            }*/
-
-
-            if (_isOpenCollectibleItems)
-            {
-                _directX3DGraphics.D2DRenderTarget.FillRectangle(new SharpDX.Mathematics.Interop.RawRectangleF(0f, 0f, _renderForm.Width, _renderForm.Height), _blackBrush); // рамка карты
-                _collectiblesInventory.DrawInventory();
-            }
-            else
-            {
-                _mainInventory.DrawInventory();
+                RenderFormResizedCallback(this, EventArgs.Empty);
+                _firstRun = false;
             }
             
-            /*if (_isMap)
-            {
-                SharpDX.Direct2D1.PathGeometry triangleGeometry = new SharpDX.Direct2D1.PathGeometry(_directX3DGraphics.D2dFactory); // тетраэдр
-                using (SharpDX.Direct2D1.GeometrySink geoSink = triangleGeometry.Open())
-                {
-                    Vector2 triangleCenter = new Vector2((_player.Position.X - _camera.Position.X) + (_renderForm.Width) / 2.0f, ((_renderForm.Height) / 2.0f) - (_player.Position.Z - _camera.Position.Z)); // 
-                    geoSink.BeginFigure(new Vector2(triangleCenter.X, triangleCenter.Y - 15f), SharpDX.Direct2D1.FigureBegin.Filled); // верхняя вершина
-                    geoSink.AddLine(new Vector2(triangleCenter.X + 15f, triangleCenter.Y + 15f)); // правая нижняя
-                    geoSink.AddLine(new Vector2(triangleCenter.X - 15f, triangleCenter.Y + 15f)); // левая вершина
-                    geoSink.EndFigure(SharpDX.Direct2D1.FigureEnd.Closed);
-                    geoSink.Close();
-                }
+            _timeHelper.Update();
+            _renderForm.Text = "FPS: " + _timeHelper.FPS.ToString();
 
-                SharpDX.Direct2D1.PathGeometry secondTriangleGeometry = new SharpDX.Direct2D1.PathGeometry(_directX3DGraphics.D2dFactory); // угол обзора наблюдателя
-                using (SharpDX.Direct2D1.GeometrySink geoSink = secondTriangleGeometry.Open())
-                {
-                    float directionalLineRadius = 75;
-
-                    var firstPoint = new RawVector2(_renderForm.Width / 2.0f + directionalLineRadius * (float)Math.Cos(Math.PI / 2.0f - _camera.Yaw - Math.PI / 3.0f),
-                        _renderForm.Height / 2.0f - directionalLineRadius * (float)Math.Sin(Math.PI / 2.0f - _camera.Yaw - Math.PI / 3.0f));
-
-                    var secondPoint = new RawVector2(_renderForm.Width / 2.0f + directionalLineRadius * (float)Math.Cos(Math.PI / 2.0f - _camera.Yaw + Math.PI / 3.0f),
-                        _renderForm.Height / 2.0f - directionalLineRadius * (float)Math.Sin(Math.PI / 2.0f - _camera.Yaw + Math.PI / 3.0f));
-
-                    geoSink.BeginFigure(new Vector2(_renderForm.Width / 2.0f, _renderForm.Height / 2.0f), SharpDX.Direct2D1.FigureBegin.Filled); // центр окружности
-                    //geoSink.AddLine(new Vector2(_renderForm.Width / 2.0f - 15.0f, _renderForm.Height / 2.0f));
-                    geoSink.AddLine(secondPoint); // левая вершина
-                    geoSink.AddLine(firstPoint); // правая вершина
-                    //geoSink.AddLine(new Vector2(_renderForm.Width / 2.0f + 15.0f, _renderForm.Height / 2.0f));
-                    geoSink.EndFigure(SharpDX.Direct2D1.FigureEnd.Closed);
-                    geoSink.Close();
-                }
+            _dxInput.Update();
 
 
-                Vector4 plotMapSize = new Vector4((_plot.Position.X - _plotSize.X) - _camera.Position.X, _camera.Position.Z - (_plot.Position.Z - _plotSize.Y),
-                    (_plot.Position.X + _plotSize.X) - _camera.Position.X, _camera.Position.Z - (_plot.Position.Z + _plotSize.Y));
+            UpdateInput();
 
+            RenderObjects();
+           
+            CheckSecondQuestCompleting();
 
+            UpdateNPC();
 
-                //_directX3DGraphics.D2DRenderTarget.Clear(Color.Black);
-
-
-
-                _directX3DGraphics.D2DRenderTarget.FillRectangle(new RawRectangleF(plotMapSize.X * 2.0f + (_renderForm.Width) / 2.0f, plotMapSize.Y * 2.0f + (_renderForm.Height) / 2.0f,
-                    plotMapSize.Z * 2.0f + (_renderForm.Width) / 2.0f, plotMapSize.W * 2.0f + (_renderForm.Height) / 2.0f), _purpleBrush); // поверхность
-                _directX3DGraphics.D2DRenderTarget.FillRectangle(new SharpDX.Mathematics.Interop.RawRectangleF(100.0f, 50.0f, _renderForm.Width - 100.0f, _renderForm.Height - 100.0f), _greenBrush); // рамка карты
-
-
-
-
-                /*_directX3DGraphics.D2DRenderTarget.DrawLine(new RawVector2(_renderForm.Width / 2.0f, _renderForm.Height / 2.0f),
-                   firstPoint, _whiteBrush, 4.0f); // направление взгляда наблюдателя
-
-
-                _directX3DGraphics.D2DRenderTarget.DrawLine(new RawVector2(_renderForm.Width / 2.0f, _renderForm.Height / 2.0f),
-                   secondPoint, _whiteBrush, 4.0f); // направление взгляда наблюдателя
-
-
-
-
-                //_directX3DGraphics.D2DRenderTarget.FillGeometry(triangleGeometry, _blueBrush); // тетраэдр
-
-                ImagingFactory imagingFactory = new ImagingFactory();
-
-                NativeFileStream fileStream = new NativeFileStream("tetrahedronchik.png",
-                    NativeFileMode.Open, NativeFileAccess.Read);
-
-                BitmapDecoder bitmapDecoder = new BitmapDecoder(imagingFactory, fileStream, DecodeOptions.CacheOnDemand);
-                BitmapFrameDecode frame = bitmapDecoder.GetFrame(0);
-
-                FormatConverter converter = new FormatConverter(imagingFactory);
-                converter.Initialize(frame, SharpDX.WIC.PixelFormat.Format32bppPRGBA);
-
-                var newBitmap = SharpDX.Direct2D1.Bitmap1.FromWicBitmap(_directX3DGraphics.D2DRenderTarget, converter);
-
-                var bitmapBrush = new BitmapBrush(_directX3DGraphics.D2DRenderTarget, newBitmap);
-
-                //_directX3DGraphics.D2DRenderTarget.FillRectangle(new RawRectangleF(100, 100, 200, 200), bitmapBrush);
-
-                var transform = _directX3DGraphics.D2DRenderTarget.Transform;
-                //new Vector2(, );
-
-                _directX3DGraphics.D2DRenderTarget.Transform = Matrix3x2.Translation((_player.Position.X - _camera.Position.X) + (_renderForm.Width) / 2.0f - newBitmap.Size.Width/2.0f, ((_renderForm.Height) / 2.0f) - (_player.Position.Z - _camera.Position.Z) - newBitmap.Size.Height / 2.0f);
-
-                _directX3DGraphics.D2DRenderTarget.DrawBitmap(newBitmap, 1.0f, SharpDX.Direct2D1.BitmapInterpolationMode.Linear);
-
-                _directX3DGraphics.D2DRenderTarget.Transform = transform;
-
-                _directX3DGraphics.D2DRenderTarget.FillGeometry(secondTriangleGeometry, _whiteBrush); // угол камеры
-
-                _directX3DGraphics.D2DRenderTarget.FillEllipse(new Ellipse(new SharpDX.Mathematics.Interop.RawVector2(_renderForm.Width / 2.0f, _renderForm.Height / 2.0f), 10.0f, 10.0f), _blueBrush); // игрок
-
-
-            }*/
-
-            if (_isDisplayingText)
-            {
-                //textLayoutText = (_secondNpcSequentialQuest.InteractableObjects[0].MeshCollider.Center).ToString() + _player.Position;
-                _textLayout = new TextLayout(_directX3DGraphics.FactoryDWrite, textLayoutText, testTextFormat, _renderForm.Width, _renderForm.Height);
-                _directX3DGraphics.D2DRenderTarget.DrawTextLayout(new RawVector2(_renderForm.Width / 2 - 400, _renderForm.Height / 2 - 100), _textLayout, _whiteBrush, DrawTextOptions.None);
-            }
-
-            _directX3DGraphics.D2DRenderTarget.EndDraw();
-            _directX3DGraphics.SwapChain.Present(0, PresentFlags.None);
-
-
-            //_directX3DGraphics.D2dContext.DrawRectangle(new SharpDX.Mathematics.Interop.RawRectangleF(
-            //    _renderForm.Left + 100.0f, _renderForm.Top + 100.0f, _renderForm.Right - 100.0f, _renderForm.Bottom - 100.0f), _blackBrush);
-            // _directX3DGraphics.D2dContext.EndDraw();
-        }
-
-        private void CheckPositions()
-        {
-            var a = _camera.Roll;
-            var b = _camera.Pitch; // наклон по y (вокруг x)
-            var c = _camera.Yaw; // поворот по x (вокруг y)
-        }
-
-        private void PressKeyboard()
-        {
+            DrawHUD();
         }
 
         public static Vector3 ToDecart(Vector3 isometric)
